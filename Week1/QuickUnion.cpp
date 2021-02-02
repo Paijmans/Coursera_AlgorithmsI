9841f2e78d1250da179a85b92a141975460bde20
Joris Paijmans <joris.paijmans@planet.nl>
2021-02-02 19:08:32 +0100

    Working version of QuickUnion!

--- Week1/QuickUnion.cpp
+++ Week1/QuickUnion.cpp
@@ -1,17 +1,72 @@
-// QuickUnion class bij me.
+// QuickUnion class method definions.
+
 #include <iostream>
 #include <vector>
 #include <string>
 
+#include "QuickUnion.hpp"
+
 using namespace std;
 
-class QuickUnion{
+// Connect elements p and q.
+void QuickUnion::Union(const unsigned& p, const unsigned& q)
+{
+    unsigned root_p(Root(p));
+    unsigned root_q(Root(q));
 
+    // If p and q are connected, do nothing.
+    if (root_p == root_q) return;
 
-    QuickUnion(uint N){
-
-        
-
+    // Change the root of the smaller connected component to that of the larger one.
+    // Update size of new connected component.
+    if (connected_size[root_p] < connected_size[root_q])
+    {
+        connected[root_p] = root_q;
+        connected_size[root_q] += connected_size[root_p];
+        connected_size[root_p] = 0;
+    }
+    else
+    {
+        connected[root_q] = root_p;
+        connected_size[root_p] += connected_size[root_q];
+        connected_size[root_q] = 0;
     }
 
+
+}
+
+// Check whether p and q are in the same connected component.
+bool QuickUnion::Search(const unsigned& p, const unsigned& q)
+{
+    // Two elements are in the same connected component if they have the same root.
+    return Root(p) == Root(q);
+}
+
+// Find the root of p.
+// Updates all references to grandparent along the search for speedup.
+unsigned QuickUnion::Root(unsigned i)
+{
+    // Move through the tree until you reach an element that refers to itself.
+    while( connected[i] != i)
+    {
+        connected[i] = connected[connected[i]];
+        i = connected[i];
+    } 
+    return i;
+}
+
+// Print number of elements in each connected component.
+void QuickUnion::PrintConnected()
+{
+    unsigned id(0);
+    unsigned tot(0);
+    for(const auto& cc_size : connected_size){
+
+        if(cc_size > 0)
+            cout << id << "\t" << cc_size << endl;
+    
+        tot += cc_size;
+        id++;
+    }
+    cout << "Total number of elements: " << tot << endl;
 }
--- Week1/QuickUnion.hpp
+++ Week1/QuickUnion.hpp
@@ -1,21 +1,47 @@
-// QuickUnion class bij me.
+#ifndef QUICKUNION_H
+#define QUICKUNION_H
 
+// QuickUnion class bij me.
 #include <vector>
 
 class QuickUnion{
 
-    uint N;
-    vector<int> connected;
+    // Number of elements.
+    unsigned N;
+    // Connected component of each element.
+    std::vector<int> connected;
+    // Size of each connected component.
+    std::vector<int> connected_size;   
 
     public:
-
-    QuickUnion(uint n) N(n)
+    // Constructor of QuickUnion.
+    QuickUnion(unsigned n): N(n)
     {
-
-        
+        // Initialized vector of connected components.
+        connected.resize(N);
+        connected_size.resize(N);
+        for(unsigned i=0; i<N; i++) 
+        {
+            // Every element is its own connected component.
+            connected[i] =  i;
+            // Every connected component has size 1.
+            connected_size[i] = 1;
+        }
     }
-        
 
-    }
+    // Connect elements p and q.
+    void Union(const unsigned& p, const unsigned& q);
 
-}
+    // Check whether p and q are in the same connected component.
+    bool Search(const unsigned& p, const unsigned& q);
+
+    // Find the root of p.
+    unsigned Root(unsigned i);
+
+    // Print number of elements in each connected component.
+    void PrintConnected();
+};
+
+
+
+#endif
